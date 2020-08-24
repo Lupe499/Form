6fd123b2732fba23749b291c2c5ad32afdff11db
Lucas Pedersen <lucas.a.pedersen@gmail.com>
2020-08-24 14:11:37 +0200

    first

---
+++ java.js
@@ -0,0 +1,34 @@
+function WhoIsMrGay(){
+    console.log("Mr Gay is Christian")
+}
+
+let form = document.getElementsByTagName("form")[0];
+let statsUsername = document.querySelector(".statusMessage.username")
+let statspassword = document.querySelector(".statusMessage.password")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let status = true;
+
+    statsUsername.innerText = "";
+
+    if (form.username.value == "") {
+        statsUsername.innerText = "du skal udfylde username";
+        status = false;
+    }
+    
+    
+    statspassword.innerText = "";
+    
+    if (form.password.value == "") {
+        statspassword.innerText = "du skal udfylde password";
+        status = false;
+    }
+
+    if (status == false) {
+        return
+    }
+
+    fetch("login.php")
+});
